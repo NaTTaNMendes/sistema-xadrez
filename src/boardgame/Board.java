301c5626586269a303144e789b95d646e80b3387
NaTTaNMendes <nattan078@gmail.com>
2021-03-02 00:58:52 -0300

    Peças colocadas no tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,15 +1,15 @@
 package boardgame;
 
 public class Board {
-	
+
 	private Integer rows;
 	private Integer columns;
 	private Piece[][] pieces;
-	
+
 	public Board(Integer rows, Integer columns) {
 		this.rows = rows;
 		this.columns = columns;
-		pieces = new Piece [rows][columns];
+		pieces = new Piece[rows][columns];
 	}
 
 	public Integer getRows() {
@@ -27,14 +27,17 @@ public class Board {
 	public void setColumns(Integer columns) {
 		this.columns = columns;
 	}
-	
-	public Piece piece (int row, int column) {
+
+	public Piece piece(int row, int column) {
 		return pieces[row][column];
 	}
-	
-	public Piece piece (Position position) {
-		return pieces [position.getRow()][position.getColumn()];
+
+	public Piece piece(Position position) {
+		return pieces[position.getRow()][position.getColumn()];
 	}
-	
-	
+
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
+	}
 }
